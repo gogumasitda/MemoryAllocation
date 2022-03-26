de02d9b7cbf77f1810c1ade376301040892a82d6
gogumassfay <gogumassafy@gmail.com>
2022-03-26 18:26:00 +0900

    feat: Add destroy B object
    At first, it checks A's reference count then decrese it.
    If any problem, it returns fail signal

--- MemoryAllocation/struct_B.c
+++ MemoryAllocation/struct_B.c
@@ -17,14 +17,21 @@ void initialize_datas_of(B* B_object, uint8_t A_object_index) {
 }
 
 bool destory_this(B* B_object) {
+	bool isSuccessToDecrease = decrease_reference_count_of(B_object->A_index);
 
+	if (isSuccessToDecrease == false) return false;
+
+	free(B_object);
+
+	return true;
 }
 
 bool decrease_reference_count_of(uint8_t A_object_index) {
-
+	// TODO: implement after merging "feature/struct_A"
+	return true;
 }
 
 bool count_data_of(B* b_object, uint64_t(*your_calc_logic)(char*)) {
-
+	// TODO: implemnt after adding set value size at size_type_definition.h
 	return true;
 }
