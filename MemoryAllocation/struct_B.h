aa9ca33cebce95d6d1243ae3fe7d9dfa25d4f4c1
gogumassfay <gogumassafy@gmail.com>
2022-03-26 18:08:35 +0900

    feat: declare struct B and related methods

---
+++ MemoryAllocation/struct_B.h
@@ -0,0 +1,13 @@
+#pragma once
+#include <stdint.h>
+#include <stdbool.h>
+
+typedef struct _B {
+	uint8_t A_index;
+	uint64_t count_data;
+} B;
+
+bool create_at(B* B_object, uint8_t A_object_index);
+bool destory_this(B* B_object);
+bool decrease_reference_count_of(uint8_t A_object_index);
+bool count_data_of(B *b_object, uint64_t (*your_calc_logic)(char *));
