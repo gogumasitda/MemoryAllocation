bbd279da9722b9ff75351277f244174885969158
gogumassfay <gogumassafy@gmail.com>
2022-03-26 18:15:40 +0900

    feat: add creating new B object
    1. create new B by malloc.
    2. If you want to reduce runtime, then change to use memory pool.

---
+++ MemoryAllocation/struct_B.c
@@ -0,0 +1,29 @@
+#pragma once
+#include "struct_B.h"
+
+void initialize_datas_of(B *B_object, uint8_t A_object_index) {
+	B_object->A_index = A_object_index;
+	B_object->count_data = 0;
+}
+
+bool create_at(B* B_object, uint8_t A_object_index) {
+	B_object = (B*) malloc(sizeof(B));
+
+	if (B_object == 0) return false;
+
+	initialize_datas_of(B_object);
+
+	return true;
+}
+
+bool destory_this(B* B_object) {
+}
+
+bool decrease_reference_count_of(uint8_t A_object_index) {
+
+}
+
+bool count_data_of(B* b_object, uint64_t(*your_calc_logic)(char*)) {
+
+	return true;
+}
