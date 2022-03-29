5635927aa78a2284bdb9226d655546ccb8bc7993
gogumassfay <gogumassafy@gmail.com>
2022-03-29 23:03:32 +0900

    refactor: refactor function related to B
    1. create_B_refer function now returns B pointer.
    2. change create B object function name to create_B_refer.
    3. change destroy B object function name to destroy_B.
    4. remove check logic in function, count_data_of because calced_data is unsigned.

--- MemoryAllocation/struct_B.c
+++ MemoryAllocation/struct_B.c
@@ -1,14 +1,14 @@
 #pragma once
 #include "struct_B.h"
 
-bool create_at(B* B_object, uint8_t A_object_index) {
-	B_object = (B*) malloc(sizeof(B));
+B *create_B_refer(uint8_t A_object_index) {
+	B *B_object = (B*) malloc(sizeof(B));
 
-	if (B_object == 0) return false;
+	if (B_object == 0) return 0;
 
 	initialize_datas_of(B_object, A_object_index);
 
-	return true;
+	return B_object;
 }
 
 void initialize_datas_of(B* B_object, uint8_t A_object_index) {
@@ -17,7 +17,7 @@ void initialize_datas_of(B* B_object, uint8_t A_object_index) {
 	increase_reference_count_of(A_object_index);
 }
 
-bool destory_this(B* B_object) {
+bool destory_B(B* B_object) {
 	bool isSuccessToDecrease = decrease_reference_count_of(B_object->A_index);
 
 	if (isSuccessToDecrease == false) return false;
@@ -53,7 +53,6 @@ bool count_data_of(B* b_object, uint64_t(*your_calc_logic)(char*)) {
 
 	uint64_t calced_data = your_calc_logic(reference->setting_value);
 
-	if (calced_data < 0) return false;
 	b_object->count_data = calced_data;
 	return true;
 }
--- MemoryAllocation/struct_B.h
+++ MemoryAllocation/struct_B.h
@@ -9,9 +9,9 @@ typedef struct _B {
 	uint64_t count_data;
 } B;
 
-bool create_at(B* B_object, uint8_t A_object_index);
+B * create_B_refer(uint8_t A_object_index);
 void initialize_datas_of(B* B_object, uint8_t A_object_index);
-bool destory_this(B* B_object);
+bool destory_B(B* B_object);
 bool increase_reference_count_of(uint8_t A_object_index);
 bool decrease_reference_count_of(uint8_t A_object_index);
 bool count_data_of(B *b_object, uint64_t (*your_calc_logic)(char *));
